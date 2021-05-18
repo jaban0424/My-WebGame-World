ad973e8f8d6373a05a6c7d6aeec6c9706671ebb0
정집인 <67492641+jaban0424@users.noreply.github.com>
2021-05-18 12:26:25 +0900

    Create common.js

---
+++ common.js
@@ -0,0 +1,32 @@
+function login(){
+    var id=document.querySelector('#id');
+    var pw=document.querySelector('#pw');
+
+    if(id.value==""){
+        alert("아이디를 입력해 주십시오!")
+    }
+    else if(pw.value==""){
+        alert("비밀번호를 입력해 주십시오!")
+    }
+    else{
+        location.href='main.html';
+    }
+}
+
+function create_id(){
+    var id=document.querySelector('#id');
+    var pw=document.querySelector('#pw');
+    var r_pw=document.querySelector('#r_pw');
+
+    if(id.value==""||pw.value==""||r_pw.value==""){
+        alert("모든 정보를 기입해 주십시오!")
+    }
+    else{
+        if(pw.value!==r_pw.value){
+            alert("비밀번호를 확인해주세요.")
+        }
+        else{
+            location.href='login.html';
+        }
+    }
+}
