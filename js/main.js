555b63c9bc0bb20395bc773faf6769a260b8ef1d
정집인 <67492641+jaban0424@users.noreply.github.com>
2021-05-21 16:58:29 +0900

    remove testing code

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ var rest=500000000;
 var bet=0;
 
 function betting(){
-    alert("1단계 작동성공");
+    //alert("1단계 작동성공");
     var ran;
     bet=document.getElementById("bet_m").value;
     alert(bet);
     rest-=bet;
     ran=Math.floor(Math.random()*1000)%4;
-    alert("2단계 작동성공"+rest);
+    //alert("2단계 작동성공"+rest);
     document.getElementById("dice").innerText="주사위 결과 : "+ran+"!!";
-    alert("2.5단계 작동성공"+rest);
+    //alert("2.5단계 작동성공"+rest);
     switch(ran){
         case 0:
             bet=0;
@@ -36,6 +36,6 @@ function betting(){
             rest+=bet;
             document.getElementById("res").innerText="2배를 얻었습니다!!!!!!";
         }
-        alert("3단계 작동성공"+rest);
+        //alert("3단계 작동성공"+rest);
     document.getElementById("money").innerText="현재 자산 : "+rest+"원";
 }
