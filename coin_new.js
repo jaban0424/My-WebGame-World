9b92e7b4ff2daccc756789bc542bd7c728ddfe17
정집인 <67492641+jaban0424@users.noreply.github.com>
2021-07-17 21:16:37 +0900

    Update coin_new.js

--- coin_new.js
+++ coin_new.js
@@ -28,11 +28,11 @@ var doge=new Coin({name : doge , price : dogep , number : 0 , own : 0 , nid:"dog
 alert('3완료');
 
 function purchase(coin){
-    alert('4완료');
+    alert("4완료");
     number=document.getElementById(coin.nid);
-    alert('5완료');
+    alert('5완료 '+number);
     money-=number*coin.price;
-    alert('6완료');
+    alert('6완료'+coin.price+money);
     document.getElementById("money").innerText=money;
     alert('7완료');
 }
