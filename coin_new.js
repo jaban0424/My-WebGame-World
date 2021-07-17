6e6a9a130aa7285fe076f2df06a837c5207f80b5
정집인 <67492641+jaban0424@users.noreply.github.com>
2021-07-17 22:02:19 +0900

    Update coin_new.js

--- coin_new.js
+++ coin_new.js
@@ -8,8 +8,6 @@ var bitp=43902824;
 
 var number=0;
 
-alert('1완료');
-
 class Coin {
     constructor({name,price,number,own,nid}) { //nid는 html에서 number input에서 받은 그 코인의 매수 개수 아이디
         this.name = name;
@@ -20,19 +18,23 @@ class Coin {
     }
 }
 
-alert('2완료');
-
-var bit=new Coin({name : bit , price : bitp , number : 0 , own : 0 , nid:"bit_n"})
-var doge=new Coin({name : doge , price : dogep , number : 0 , own : 0 , nid:"doge_n"})
-
-alert('3완료');
+var bit=new Coin({name : bit , price : bitp , number : 0 , own : 0 , nid:"idbit"})
+var doge=new Coin({name : doge , price : dogep , number : 0 , own : 0 , nid:"iddoge"})
 
 function purchase(coin){
-    alert("4완료");
-    number=document.getElementById(coin.nid).value;
-    alert('5완료 '+number);
+    number=document.getElementById(coin.nid+"pn").value;
     money-=number*coin.price;
-    alert('6완료'+coin.price+money);
     document.getElementById("money").innerText=money;
-    alert('7완료');
+    coin.own+=parseInt(number); //도대체 여기서 왜 number를 정수로 만들어줘야하는 거임??
+    document.getElementById(coin.nid+"o").innerText=coin.own;
+    document.getElementById(coin.nid+"pn").value="0";//공백만들기
+}
+
+function sell(coin){
+    number=document.getElementById(coin.nid+"sn").value;
+    money+=number*coin.price;
+    document.getElementById("money").innerText=money;
+    coin.own-=parseInt(number);
+    document.getElementById(coin.nid+"o").innerText=coin.own;
+    document.getElementById(coin.nid+"sn").value="0";//공백만들기
 }
