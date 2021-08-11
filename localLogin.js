a853c0544145ca45aa0b8cb73b16525a014f8a6b
염성현 <80742504+cis-04@users.noreply.github.com>
2021-08-11 17:40:24 +0900

    로그인
    '전면등교'

---
+++ localLogin.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const loginBtn = document.getElementById('loginBtn');
+let idValue = document.querySelector('#id').value;
+let pwValue=document.querySelector('#pw').value;
+
+function login(){
+    if(idValue==""){
+        alert("아이디를 입력해 주십시오!")
+    }
+    else if(pwValue==""){
+        alert("비밀번호를 입력해 주십시오!")
+    }
+    else{
+        let ID = localStorage.getItem('id');
+        let PW = localStorage.getItem('pw');
+        if(idValue === ID && pwValue === PW) {
+            alert('login 성공');
+            location.href='main.html';
+        }
+        else alert('일치하는 정보가 없습니다.');
+    }
+}
+
+loginBtn.addEventListener('click',login);
