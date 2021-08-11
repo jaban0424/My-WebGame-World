f28e47778a3bf09b76a107ae2cc593fc6462712c
염성현 <80742504+cis-04@users.noreply.github.com>
2021-08-11 17:59:56 +0900

    a
    a

--- js/login.js
+++ js/login.js
@@ -1,22 +1,9 @@
-function login(){
-    var id=document.querySelector('#id');
-    var pw=document.querySelector('#pw');
-
-    if(id.value==""){
-        alert("아이디를 입력해 주십시오!")
-    }
-    else if(pw.value==""){
-        alert("비밀번호를 입력해 주십시오!")
-    }
-    else{
-        location.href='main.html';
-    }
-}
+'use strict';
 
 function create_id(){
-    var id=document.querySelector('#id');
-    var pw=document.querySelector('#pw');
-    var r_pw=document.querySelector('#r_pw');
+    let id=document.querySelector('#id');
+    let pw=document.querySelector('#pw');
+    let r_pw=document.querySelector('#r_pw');
 
     if(id.value==""||pw.value==""||r_pw.value==""){
         alert("모든 정보를 기입해 주십시오!")
@@ -27,7 +14,9 @@ function create_id(){
         }
         else{
             alert("회원가입을 완료했습니다.")
-            location.href='.\index.html';
+            localStorage.setItem('id',id.value);
+            localStorage.setItem('pw',pw.value);
+            location.href='index.html';
         }
     }
 }
