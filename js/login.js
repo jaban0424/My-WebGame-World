b9316b16d3221fbca5364dbc2e9541cb335eb209
염성현 <80742504+cis-04@users.noreply.github.com>
2021-08-11 18:09:12 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -18,7 +18,6 @@ function create_id(){
             alert("회원가입을 완료했습니다.")
             localStorage.setItem('id',id.value);
             localStorage.setItem('pw',pw.value);
-            location.href='index.html';
         }
     }
 }
